09c74559f5711856ba60c94b24bd03e301effee0
Theo <ekunnusitheophilus@gmail.com>
2023-08-03 14:17:57 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,3 +29,23 @@ def filter_datum(
             '(?<={}=)[^{}]+'.format(field, separator),
             '{}'.format(redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        logging.basicConfig(level=record.levelname, format=self.FORMAT)
+        logger = logging.getLogger(record.name)
+        msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        msg = re.sub(r';', '; ', msg)
+        logger.info(msg)
