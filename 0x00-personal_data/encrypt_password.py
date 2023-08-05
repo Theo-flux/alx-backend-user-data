7d0f9256dd7bf49662f90e357f97124c43b2d9c4
Theo <ekunnusitheophilus@gmail.com>
2023-08-05 19:42:45 +0100

    Encryption passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+password encryption module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    hash password by salting and encryption
+
+    Args:
+        password (str): _description_
+
+    Returns:
+        bytes: _description_
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
