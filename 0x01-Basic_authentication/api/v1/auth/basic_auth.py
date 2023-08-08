01b3768b9c1a1a009c4285c3fc323b1deb5154f7
Theo <ekunnusitheophilus@gmail.com>
2023-08-08 03:41:30 +0100

    Basic user credntials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,3 +54,26 @@ class BasicAuth(Auth):
             return res.decode("utf-8")
         except Exception as err:
             return None
+
+    def extract_user_credentials(
+        self,
+        decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        extract credentials
+
+        Args:
+            self (_type_): _description_
+            str (_type_): _description_
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        else:
+            credentials = tuple(decoded_base64_authorization_header.split(":"))
+            return credentials
