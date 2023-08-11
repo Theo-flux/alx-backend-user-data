578881c8563f10a407e72e1bd90905aa86d6b1ad
Theo <ekunnusitheophilus@gmail.com>
2023-08-11 10:43:38 +0100

    phew! finally gave my user its cookies

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Module for session auth views"""
+from flask import request, jsonify
+from api.v1.views import app_views
+from models.user import User
+
+
+from os import getenv
+
+
+@app_views.route("/auth_session/login", methods=['POST'], strict_slashes=False)
+def session_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or len(email) == 0:
+        return jsonify({'error': "email missing"}), 400
+
+    if password is None or len(password) == 0:
+        return jsonify({'error': "password missing"}), 400
+
+    session_user_instance = User.search({'email': email})
+
+    if not session_user_instance:
+        return jsonify({"error": "no user found for this email"}), 404
+    else:
+        for session_user in session_user_instance:
+            is_valid = session_user.is_valid_password(password)
+
+            if not is_valid:
+                return jsonify({"error": "wrong password"}), 401
+            else:
+                from api.v1.app import auth
+
+                user_id = getattr(session_user, 'id')
+                session_id = auth.create_session(user_id)
+
+                response = jsonify(session_user.to_json())
+                response.set_cookie(getenv("SESSION_NAME"), session_id)
+
+                return response
+
+    return jsonify({'email': email, 'password': password})
