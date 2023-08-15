522feb965931e452c621832c0568caaa6e55b23b
Theo <ekunnusitheophilus@gmail.com>
2023-08-15 11:57:10 +0100

    feat:user session auth with db and exp

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -8,5 +8,6 @@ class UserSession(Base):
 
     def __init__(self, *args: list, **kwargs: dict):
         """instance initialization"""
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
