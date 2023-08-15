a7c7341fd839ea029038229f3f14feed112a28f7
Theo <ekunnusitheophilus@gmail.com>
2023-08-15 11:52:24 +0100

    feat:user session auth with db and exp

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""session db authentication module"""
+from datetime import datetime, timedelta
+
+from .session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """Session DB Auth class object"""
+
+    def create_session(self, user_id=None):
+        """create session id"""
+        session_id = super().create_session(user_id)
+
+        if session_id is None:
+            return None
+
+        UserSession(user_id=user_id, session_id=session_id).save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """get user id by session id"""
+        try:
+            session_list = UserSession.search({'session_id': session_id})
+        except Exception:
+            return None
+        if len(session_list) <= 0:
+            return None
+        cur_time = datetime.now()
+        created_at = session_list[0].created_at
+        duration = timedelta(seconds=self.session_duration)
+        limit = session_list[0].created_at + duration
+        if limit < cur_time:
+            return None
+        return session_list[0].user_id
+
+    def destroy_session(self, request=None):
+        session_id = self.session_cookie(request)
+        try:
+            session_list = UserSession.search({'session_id': session_id})
+        except Exception:
+            return False
+        if len(session_list) <= 0:
+            return False
+        session_list[0].remove()
+        return True
---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""user session module"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """ user session class object"""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """instance initialization"""
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
