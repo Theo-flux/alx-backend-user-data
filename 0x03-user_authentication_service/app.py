2e82d65622453a661d88a1cf15b0dbbd6b229be8
Theo <ekunnusitheophilus@gmail.com>
2023-08-17 10:24:07 +0100

    basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/env/bin python3
+"""basic flask app module"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes='/')
+def index():
+    """return joson payload"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
