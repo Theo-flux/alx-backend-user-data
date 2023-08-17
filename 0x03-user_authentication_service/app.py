210ade49b691206398307f0c545ffd6a504ca518
Theo <ekunnusitheophilus@gmail.com>
2023-08-17 13:45:03 +0100

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """basic flask app module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from sqlalchemy.orm.exc import NoResultFound
 
 from auth import Auth
@@ -35,6 +35,28 @@ def create_user():
         return jsonify({"message": "email already registered"})
     else:
         return jsonify({"email": f"{mail}", "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /users
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+      - 400 if can't create the new User
+    """
+    mail = request.form['email']
+    pwd = request.form['password']
+
+    flag = AUTH.valid_login(email=mail, password=pwd)
+    if flag:
+        session_id = AUTH.create_session(mail)
+        res = jsonify({"email": f"{mail}", "message": "user created"})
+        res.set_cookie("session_id", session_id)
+        return res
+    return abort(401)
 
 
 if __name__ == "__main__":
